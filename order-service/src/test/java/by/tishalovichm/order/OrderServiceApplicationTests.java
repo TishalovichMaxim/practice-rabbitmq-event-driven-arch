2c8d11d8da19b8f7dc7f5a7c40df59ba97e9d609
Tishalovich Maxim <tishalovichm@gmail.com>
2024-06-20 20:27:19 +0300

    Init spring boot order microservice

---
+++ order-service/src/test/java/by/tishalovichm/order/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.tishalovichm.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
